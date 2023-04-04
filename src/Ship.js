a7538b82556e4928c3678123f53d2d46ed13dd72
Jenny Rafferty <raffertyjenny@gmail.com>
2023-04-04 21:09:02 +0100

    added itinerary tests

--- src/Ship.js
+++ src/Ship.js
@@ -1,16 +1,30 @@
 const Port = require('../src/Port');
+const Itinerary = require('../src/Itinerary');
 
 class Ship {
-    constructor(currentPort){
-        this.currentPort = currentPort;
+    constructor(itinerary){
+        this.itinerary = itinerary;
+        this.currentPort = itinerary.ports[0];
+        this.previousPort = null;
     };
     setSail() {
-    return this.currentPort;
+    const itinerary = this.itinerary;
+    const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+
+    if (currentPortIndex === (itinerary.ports.length - 1)) {
+        throw new Error('End of itinerary reached');
     }
-    dock(port) {
-        this.currentPort = port;
+
+    this.previousPort = this.currentPort;
+    this.currentPort = null;
+    }
+
+    dock() {
+    const itinerary = this.itinerary;
+    const previousPortIndex =itinerary.ports.indexOf(this.previousPort)
+
+    this.currentPort = itinerary.ports[previousPortIndex + 1]
     }
 };
-
 
 module.exports = Ship  
