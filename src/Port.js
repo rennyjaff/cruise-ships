9540062cf05c00b25b5813604ca238d6ebd18ccc
Jenny Rafferty <raffertyjenny@gmail.com>
2023-04-16 15:35:25 +0100

    added ports have ships

--- src/Port.js
+++ src/Port.js
@@ -1,7 +1,15 @@
-class Port {
-    constructor(name){
-        this.name = name;
-    };
-};
+function Port(name) {
+    this.name = name;
+    this.ships = [];
+  }
+
+  Port.prototype = {
+    addShip(ship) {
+      this.ships.push(ship);
+    },
+    removeShip(ship) {
+      this.ships = this.ships.filter(shipDeparted => shipDeparted !== ship);
+    },
+  };
 
 module.exports = Port
--- src/Ship.js
+++ src/Ship.js
@@ -6,24 +6,23 @@ class Ship {
         this.itinerary = itinerary;
         this.currentPort = itinerary.ports[0];
         this.previousPort = null;
+        this.currentPort.addShip(this);
     };
     setSail() {
     const itinerary = this.itinerary;
     const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
-
     if (currentPortIndex === (itinerary.ports.length - 1)) {
         throw new Error('End of itinerary reached');
     }
-
+    this.currentPort.removeShip(this);
     this.previousPort = this.currentPort;
     this.currentPort = null;
     }
-
     dock() {
     const itinerary = this.itinerary;
-    const previousPortIndex =itinerary.ports.indexOf(this.previousPort)
-
-    this.currentPort = itinerary.ports[previousPortIndex + 1]
+    const previousPortIndex =itinerary.ports.indexOf(this.previousPort);
+    this.currentPort = itinerary.ports[previousPortIndex + 1];
+    this.currentPort.addShip(this);
     }
 };
 
